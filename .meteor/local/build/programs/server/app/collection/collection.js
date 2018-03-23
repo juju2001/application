41bab823dc64179fa84eb3e1123515bda487bba7
Barras Julien <julien.barras@swisscom.com>
2018-03-23 13:09:57 +0100

    Merge branch 'settings' into master, up to date with last code

--- .meteor/local/build/programs/server/app/collection/collection.js
+++
@@ -1,6 +0,0 @@
-(function(){Inscription = new Mongo.Collection("inscription");
-Connexion = new Mongo.Collection("connexion");
-Message = new Mongo.Collection("message");
-Contact = new Mongo.Collection("contact");
-
-}).call(this);
